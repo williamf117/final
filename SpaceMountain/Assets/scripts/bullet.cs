b057b4a396ab972d2ab3d85999ab0199361d6466
wforrest117 <williamforrest117@gmail.com>
2019-02-03 16:22:58 -0700

    eanamy Ai started in battle

---
+++ SpaceMountain/Assets/scripts/bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    float x;                // x and y coordinates for the bullet movement 
+    float y;
+    float bulletSpeed = .5f;  //speed of the bullet
+    float removetime = 2;   //time until the bullet is removed 
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); //get the rigid body 
+        x = Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z) * bulletSpeed; //determine x and y components
+        y = Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.z) * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the bullet appropriately
+        rb2d.MovePosition(new Vector2(transform.position.x + x, transform.position.y + y));
+
+        removetime -= Time.deltaTime;
+
+        //delete the bullet after a certain time 
+        if (removetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- SpaceMountain/Assets/scripts/eanamy/railgun.cs
+++ SpaceMountain/Assets/scripts/eanamy/railgun.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class railgun : MonoBehaviour
 {
     [SerializeField]
-    float maxrainge = 15, minrainge = 5;//effective rainge of the gun. 
-    bool targetInrainge = false;
+    float maxrainge = 15, minrainge = 5, moveSpeed, cooldown=3;//effective rainge of the gun. 
+    [SerializeField]
+    GameObject bullet;
+    Rigidbody2D rb2d;
     GameObject[] playerships;
     GameObject currenttarget; 
     float rotationspeed=3;
     bool inrainge=false;
+    bool oncooldown = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
         
     }
 
@@ -26,25 +30,55 @@ public class railgun : MonoBehaviour
             currenttarget = FindTarget();
         }
         float rainge = Vector2.Distance(transform.position, currenttarget.transform.position);
-       ;
-        if (rainge > minrainge && rainge < maxrainge)
+     
+
+        if (rainge > minrainge && rainge<maxrainge)
         {
             inrainge = true;
         }
-        else
+        else if(rainge < minrainge&&rainge<maxrainge)
         {
             inrainge = false;
-        } Debug.Log(inrainge );
+            OpenRainge();
+        }
+        else if(rainge > maxrainge)
+        {
+            inrainge = false;
+            CloseToRaing();
+        }
+
+       
 
 
         //rotate to face the target if inrainge
         if (inrainge)
         {
             //turn to face target
+            rb2d.velocity = Vector2.zero;
             Vector3 vectorToTarget = currenttarget.transform.position - transform.position;
             float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotationspeed);
+
+            //fire if you can
+            if (!oncooldown)
+            {
+               GameObject round= Instantiate(bullet, transform.position, transform.rotation);
+                oncooldown = true;
+                Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(),round.GetComponent<CircleCollider2D>());
+
+
+            }
+
+            if (oncooldown)
+            {
+                cooldown -= Time.deltaTime;
+                if (cooldown < 0)
+                {
+                    oncooldown = false;
+                    cooldown = 3;
+                }
+            }
         }
     }
 
@@ -55,5 +89,23 @@ public class railgun : MonoBehaviour
         Debug.Log(playerships[target]);
         return playerships[target];
         
+    }
+
+    /// <summary>
+    /// a function to close to rainge 
+    /// </summary>
+    void CloseToRaing()
+    {
+        Vector2 vectorToTarget = currenttarget.transform.position - transform.position;
+        // transform.position = Vector3.MoveTowards(transform.position, currenttarget.transform.position, Time.deltaTime * moveSpeed);
+        rb2d.velocity = vectorToTarget;
+     
+    }
+    /// <summary>
+    /// a function to open rainge 
+    /// </summary>
+    void OpenRainge()
+    {
+
     }
 }
